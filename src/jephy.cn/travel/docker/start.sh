93fb821e4f88cfff266ddd6573dfa5adabfd67b1
jiangxiangxuan <jiangxiangxuan@126.com>
2019-08-31 12:01:30 +0800

    docker

--- src/jephy.cn/travel/docker/start.sh
+++ src/jephy.cn/travel/docker/start.sh
@@ -4,7 +4,7 @@ mkdir /data/logs -p
 
 ln -s /opt/apps/travel/nginx/include.conf /etc/nginx/conf.d/travel_include.conf
 cp -rf /opt/apps/travel/nginx/nginx.conf /etc/nginx/nginx.conf
-cp -rf /opt/apps/travel/docker/supervisord/*  /etc/supervisord/
+cp -rf /opt/apps/travel/docker/supervisord/*  /etc/supervisord.d/
 cp -rf /opt/apps/travel/etc/travel_api.toml /etc/travel_api.toml
 
 #service nginx restart
