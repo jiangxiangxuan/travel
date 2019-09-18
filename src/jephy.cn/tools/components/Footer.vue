eabc361f2702365500230208817ef43de9a996ea
jiangxiangxuan <jiangxiangxuan@wps.cn>
2019-09-18 18:05:11 +0800

    ba

--- src/jephy.cn/tools/components/Footer.vue
+++ src/jephy.cn/tools/components/Footer.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="footer">
     Copyright ©2019-2029 iteam. All Rights Reserved. Current version is 1.0.0.
+    <a href="www.beian.miit.gov.cn" target="_blank">粤ICP备19053447号</a>
   </div>
 </template>
